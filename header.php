3a67d4d45827c92b25e29bed8362ed28d52e8088
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-07-13 11:31:20 +0300

    Clean up header.php file.

--- header.php
+++ header.php
@@ -29,32 +29,33 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'sanse' ); ?></a>
 
-	
-		<header id="masthead" class="site-header" role="banner">
-			<div class="wrapper">
+	<header id="masthead" class="site-header" role="banner">
+		<div class="wrapper">
 			
-				<div class="site-branding">
-					<?php
-					if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-					endif;
-
-					$description = get_bloginfo( 'description', 'display' );
-						if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-					<?php
-					endif; ?>
-				</div><!-- .site-branding -->
-
+			<div class="site-branding">
 				<?php
-					get_template_part( 'menus/menu', 'primary' ); // Loads the menus/menu-primary.php template.
+				if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+				endif;
+
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
+					endif;
 				?>
+			</div><!-- .site-branding -->
+
+			<?php
+				get_template_part( 'menus/menu', 'primary' ); // Loads the menus/menu-primary.php template.
+			?>
 		
-			</div><!-- .wrapper -->
-		</header><!-- #masthead -->
+		</div><!-- .wrapper -->
+	</header><!-- #masthead -->
+	
 	<div class="hero" id="hero">
 	</div><!-- .hero -->
 
