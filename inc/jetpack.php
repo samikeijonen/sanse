29a1dbc4008d0ad6afc2eeeec1b7deeddef8070b
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-08-01 16:37:26 +0300

    Remove infinite scroll support.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -10,32 +10,12 @@
 /**
  * Jetpack setup function.
  *
- * See: https://jetpack.com/support/infinite-scroll/
  * See: https://jetpack.com/support/responsive-videos/
  */
 function sanse_jetpack_setup() {
-	// Add theme support for Infinite Scroll.
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'render'    => 'sanse_infinite_scroll_render',
-		'footer'    => 'page',
-	) );
 
 	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
+	
 }
 add_action( 'after_setup_theme', 'sanse_jetpack_setup' );
-
-/**
- * Custom render function for Infinite Scroll.
- */
-function sanse_infinite_scroll_render() {
-	while ( have_posts() ) {
-		the_post();
-		if ( is_search() ) :
-		    get_template_part( 'template-parts/content', 'search' );
-		else :
-		    get_template_part( 'template-parts/content', get_post_format() );
-		endif;
-	}
-}
