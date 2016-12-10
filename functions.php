0b3b8e00269d98818fe0669f2080c5ff3c58d06a
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-12-10 12:05:32 +0200

    esc_html() in read more link.

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ function sanse_setup() {
 		'primary' => esc_html__( 'Primary', 'sanse' ),
 		'social'  => esc_html__( 'Social Links', 'sanse' ),
 	) );
-	
+
 	// Add support for logo.
 	add_theme_support( 'custom-logo', apply_filters( 'sanse_custom_logo_arguments', array(
 		'height'      => 90,
@@ -47,7 +47,7 @@ function sanse_setup() {
 		'flex-height' => true,
 		'flex-width'  => true,
 	) ) );
-	
+
 	/*
 	 * Add support for selective refresh.
 	 *
@@ -66,7 +66,7 @@ function sanse_setup() {
 		'gallery',
 		'caption',
 	) );
-	
+
 	/*
 	 * This theme styles the visual editor to resemble the theme style,
 	 * specifically font, colors, icons, and column width.
@@ -95,7 +95,7 @@ add_action( 'after_setup_theme', 'sanse_content_width', 0 );
  */
 function sanse_widgets_init() {
 
-		register_sidebar( array(
+	register_sidebar( array(
 		'name'          => esc_html__( 'Footer widget area 1', 'sanse' ),
 		'id'            => 'footer-1',
 		'description'   => esc_html__( 'Add widgets here for footer widget area 1.', 'sanse' ),
@@ -104,7 +104,7 @@ function sanse_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
-	
+
 	register_sidebar( array(
 		'name'          => esc_html__( 'Footer widget area 2', 'sanse' ),
 		'id'            => 'footer-2',
@@ -114,7 +114,7 @@ function sanse_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
-	
+
 	register_sidebar( array(
 		'name'          => esc_html__( 'Footer widget area 3', 'sanse' ),
 		'id'            => 'footer-3',
@@ -124,7 +124,7 @@ function sanse_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
-	
+
 	register_sidebar( array(
 		'name'          => esc_html__( 'Front Page template widget area', 'sanse' ),
 		'id'            => 'front-page',
@@ -134,7 +134,7 @@ function sanse_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
-	
+
 }
 add_action( 'widgets_init', 'sanse_widgets_init' );
 
@@ -153,26 +153,26 @@ add_action( 'wp_head', 'sanse_javascript_detection', 0 );
  * Enqueue scripts and styles.
  */
 function sanse_scripts() {
-	
+
 	// Get '.min' suffix.
 	$suffix = sanse_get_min_suffix();
-	
+
 	// Add parent theme styles if using child theme.
 	if ( is_child_theme() ) {
 		wp_enqueue_style( 'sanse-parent-style', trailingslashit( get_template_directory_uri() ) . 'style' . $suffix . '.css', array(), null );
 	}
-	
+
 	// Add theme styles.
 	wp_enqueue_style( 'sanse-style', get_stylesheet_uri() );
-	
+
 	// Add theme scripts.
 	wp_enqueue_script( 'sanse-navigation', get_template_directory_uri() . '/assets/js/navigation' . $suffix . '.js', array(), '20160715', true );
-	
+
 	// Add comments script.
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-	
+
 }
 add_action( 'wp_enqueue_scripts', 'sanse_scripts' );
 
@@ -191,29 +191,29 @@ add_action( 'wp_enqueue_scripts', 'sanse_scripts' );
  * @return    string  $stylesheet_uri
  */
 function sanse_min_stylesheet_uri( $stylesheet_uri, $stylesheet_dir_uri ) {
-	
+
 	// Get the minified suffix.
 	$suffix = sanse_get_min_suffix();
-	
+
 	// Use the .min stylesheet if available.
 	if ( $suffix ) {
-		
+
 		// Remove the stylesheet directory URI from the file name.
 		$stylesheet = str_replace( trailingslashit( $stylesheet_dir_uri ), '', $stylesheet_uri );
-		
+
 		// Change the stylesheet name to 'style.min.css'.
 		$stylesheet = str_replace( '.css', "{$suffix}.css", $stylesheet );
-		
+
 		// If the stylesheet exists in the stylesheet directory, set the stylesheet URI to the dev stylesheet.
 		if ( file_exists( trailingslashit( get_stylesheet_directory() ) . $stylesheet ) ) {
 			$stylesheet_uri = esc_url( trailingslashit( $stylesheet_dir_uri ) . $stylesheet );
 		}
-		
+
 	}
-		
+
 	// Return the theme stylesheet.
 	return $stylesheet_uri;
-		
+
 }
 add_filter( 'stylesheet_uri', 'sanse_min_stylesheet_uri', 5, 2 );
 
@@ -233,10 +233,10 @@ function sanse_get_min_suffix() {
  * @since 1.1.0
  */
 function sanse_include_svg_icons() {
-	
+
 	// Define SVG sprite file.
 	$svg_icons = get_template_directory() . '/assets/images/svg-icons.svg';
-	
+
 	// If it exists, include it.
 	if ( file_exists( $svg_icons ) ) {
 		require_once( $svg_icons );
@@ -254,7 +254,7 @@ add_action( 'wp_footer', 'sanse_include_svg_icons', 9999 );
 function sanse_excerpt_more() {
 
 	/* Translators: The %s is the post title shown to screen readers. */
-	$text = sprintf( esc_attr__( 'Read more %s', 'sanse' ), '<span class="screen-reader-text">' . get_the_title() ) .  '</span>';
+	$text = sprintf( esc_html__( 'Read more %s', 'sanse' ), '<span class="screen-reader-text">' . esc_html( get_the_title() ) .  '</span>' );
 	$more = sprintf( '&hellip; <a href="%s" class="more-link">%s %s</a>', esc_url( get_permalink() ), $text, sanse_get_svg( array( 'icon' => 'next' ) ) );
 
 	return $more;
@@ -274,7 +274,7 @@ add_filter( 'excerpt_more', 'sanse_excerpt_more' );
  * @return string Menu item with possible description.
  */
 function sanse_nav_social_icons( $item_output, $item, $depth, $args ) {
-	
+
 	// Supported social icons.
 	$social_icons = apply_filters( 'sanse_nav_social_icons', array(
 		'codepen.io'      => 'codepen',
@@ -306,7 +306,7 @@ function sanse_nav_social_icons( $item_output, $item, $depth, $args ) {
 		'wordpress.com'   => 'wordpress',
 		'youtube.com'     => 'youtube',
 	) );
-	
+
 	// Change SVG icon inside social links menu if there is supported URL.
 	if ( 'social' == $args->theme_location ) {
 		foreach ( $social_icons as $attr => $value ) {
@@ -327,7 +327,7 @@ add_filter( 'walker_nav_menu_start_el', 'sanse_nav_social_icons', 10, 4 );
  * @return array
  */
 function sanse_body_classes( $classes ) {
-	
+
 	// Adds a class of group-blog to blogs with more than 1 published author.
 	if ( is_multi_author() ) {
 		$classes[] = 'group-blog';
@@ -337,17 +337,17 @@ function sanse_body_classes( $classes ) {
 	if ( ! is_singular() ) {
 		$classes[] = 'hfeed';
 	}
-	
+
 	// Add the '.custom-header-image' class if the user is using a custom header image.
 	if ( get_header_image() ) {
 		$classes[] = 'custom-header-image';
 	}
-	
+
 	// Add the '.no-header-text' class if there is no Site Title and Tagline.
 	if ( ! display_header_text() ) {
 		$classes[] = 'no-header-text';
 	}
-	
+
 	// Footer widget area count.
 	$footer_widget_count = 0;
 	if( is_active_sidebar( 'footer-1' ) ) {
@@ -359,7 +359,7 @@ function sanse_body_classes( $classes ) {
 	if( is_active_sidebar( 'footer-3' ) ) {
 		$footer_widget_count++;
 	}
-	
+
 	$classes[] = 'footer-widgets-' . $footer_widget_count;
 
 	return $classes;
